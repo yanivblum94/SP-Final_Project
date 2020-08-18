f99bca2c7351aa0288dfc0ffcd25926106e064a3
iristaub <59415717+iristaub@users.noreply.github.com>
2020-08-18 17:19:10 +0300

    Create module_alg.h

---
+++ module_alg.h
@@ -0,0 +1,10 @@
+#include "spmat.h"
+
+#ifndef MODULE_ALG_H_
+#define MODULE_ALG_H_
+
+int* devision_in_two(spmat* B^_g, int* ranks, int m, int size);
+
+int mult_vectors(const double *v1, const double *v2, int n);
+
+#endif /* MODULE_ALG_H_ */
