cb44f8c3a961bbf9b83e13d1afc1bbccdd67ac9e
iristaub <59415717+iristaub@users.noreply.github.com>
2020-08-21 18:44:54 +0300

    Update module_alg.h

--- module_alg.h
+++ module_alg.h
@@ -1,10 +1,21 @@
+/*
+ * module_alg.h
+ *
+ *  Created on: 18 באוג׳ 2020
+ *      Author: irist
+ */
 #include "spmat.h"
 
 #ifndef MODULE_ALG_H_
 #define MODULE_ALG_H_
 
-int* devision_in_two(spmat* B^_g, int* ranks, int m, int size);
+double calc_Q(int* s, spmat* B, int n);
 
 int mult_vectors(const double *v1, const double *v2, int n);
+
+int* divition_to_2(spmat* B_g_bar, int n);
+
+
+
 
 #endif /* MODULE_ALG_H_ */
