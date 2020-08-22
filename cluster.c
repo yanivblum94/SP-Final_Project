4c88b840e93107be110279a6788568b7a79f8c1a
iristaub <59415717+iristaub@users.noreply.github.com>
2020-08-22 11:16:57 +0300

    Update cluster.c

--- cluster.c
+++ cluster.c
@@ -16,14 +16,21 @@
 
 spmat* read_mat(FILE *input, int* ranks, int size){
 	int i,k,temp,j;
+	int n;
 	double* row;
 	spmat* A;
 	A = spmat_allocate(size);
-	for(i=0; i<size; i++){
-		fread(&k, sizeof(int), 1, input);
+	for(i = 0; i < size; i++){
+		n = fread(&k, sizeof(int), 1, input);
+		if(n != 1){
+			printf("Couldn't open the file");
+		}
 		ranks[i] = k;
-		row = calloc(k, sizeof(double));
-		fread(&row, sizeof(int), k, input);
+		row = (double*)calloc(k, sizeof(double));
+		n = fread(&row, sizeof(int), k, input);
+		if(n != k){
+			printf("Error in reading a row");
+		}
 		A->add_row(row);
 	}
 	return A;
@@ -41,14 +48,31 @@ int calc_M(int* ranks,int size){
 
 int main(int argc, char* argv[]){
 	FILE *input;
-	spmat* A,B;
-	int* ranks;
-	int size,m;
-	input = fopen(argv[1]);
-	fread(&size, sizeof(int), 1, input);
-	ranks = calloc(size, sizeof(int));
+	spmat *A, *B;
+	int *ranks;
+	int size,m, n;
+	input = fopen(argv[1], "r");
+	if(input == NULL){
+		printf("Error in reading the file");
+		return 1;
+	}
+	n = fread(&size, sizeof(int), 1, input);
+	if(n != 1){
+		printf("Error in reading the size of the matrix");
+	}
+	ranks = (int*)calloc(size, sizeof(int));
+	if(ranks == NULL){
+		printf("Error in allocation of memory");
+		return 1;
+	}
 	A = read_mat(input,ranks,size);
+	fclose(input);
 	m = calc_M(ranks,size);
 	B = create_B(A, ranks, m, size);
+	
+	free(ranks);
+	free_in_list(A);
+	free_in_list(B);
+	return 0;
 }
 
