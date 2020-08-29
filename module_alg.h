54dbf1ef653e2e17c2c97c5ee099a7dd32947f90
iristaub <59415717+iristaub@users.noreply.github.com>
2020-08-29 11:01:36 +0300

    Update module_alg.h

--- module_alg.h
+++ module_alg.h
@@ -9,6 +9,26 @@
 #ifndef MODULE_ALG_H_
 #define MODULE_ALG_H_
 
+typedef struct _list_of_lists{
+
+	/*node*/
+	 node node;
+
+	/*next pointer*/
+	struct _list_of_lists* next;
+
+}list_of_lists;
+
+typedef struct _node{
+
+	/*value*/
+	 int val;
+
+	/*next pointer*/
+	struct _node* next;
+
+}node;
+
 double calc_Q(int* s, spmat* B, int n);
 
 int mult_vectors(const double *v1, const double *v2, int n);
